01ea72e13de899e4a2b0af3037aa8d220d39a666
oajh04 <cloudtubekr@naver.com>
2022-01-21 21:53:46 +0900

    ⚡️ :: 소켓 플러그인 사용

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import './index.css'
 import router from './router'
+import socketPlugin from './plugins/socketPlugin'
 
 import App from './App.vue'
 const app = createApp(App);
 
 app.use(router)
+app.use(socketPlugin)
 
 app.mount('#app');
