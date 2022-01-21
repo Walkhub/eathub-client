a77c75740f735116f8c86b78efdb6e2b8f38e560
oajh04 <cloudtubekr@naver.com>
2022-01-21 22:01:36 +0900

    ⚡️ :: (#20) app

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,10 @@
 <template>
-  <MenuCard 
-    store="퀴즈노스"
-    menu="블랙 앵거스"
-    price="7,000"
-    star=""
-    count="7"
-    img="./assets/example.png"
-  />
+  <router-view/>
 </template>
 
 <script>
-import MenuCard from './components/MenuCard.vue'
-
 export default {
-  name: 'App',
-  components: {
-    MenuCard
-  }
+  name: 'App'
+
 }
 </script>
