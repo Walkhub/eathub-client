7d4435e06d2452cbc980c968f8dcb74c1d0960a0
oajh04 <cloudtubekr@naver.com>
2022-01-24 21:44:16 +0900

    ⚡️ :: (#28) 별점 ui

---
+++ src/components/Star.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="star-wrapper" :style="cssProps">
+        <input type="range" v-model="value" :style="cssProps" min=0 max=10 :disabled="!isMove"/>
+        <img src="../assets/star.png" :style="cssProps"/>
+        <input type="range" v-model="value" :style="cssProps" min=0 max=10 :disabled="!isMove"/>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'width',
+        'height',
+        'value',
+        'isMove'
+    ],
+    computed: {
+        cssProps() {
+            console.log(this.width)
+            return {
+                'width': this.width,
+                'height': this.height
+            }
+        }
+    },
+    mounted() {
+        console.log(this.value)
+    }
+    
+}
+</script>
+
+<style scoped>
+.star-wrapper {
+    width: val(--width);
+    height: val(--height);
+}
+input[type="range"] {
+    overflow: hidden;
+    width: val(--width);
+    height: val(--heigth);
+    -webkit-appearance: none;
+    background: transparent;
+    background-color: #f2f2f2;
+    z-index: 1;
+    position: absolute;
+    top: 0;
+    margin: 0px;
+}
+input[type="range"]:nth-child(3){
+    position: absolute;
+    top: 0;
+    z-index: 2000;
+    opacity: 0;
+}
+
+input[type="range"]:focus {
+	outline: none;
+}
+
+input[type="range"]::-webkit-slider-runnable-track {
+    width: val(--width);
+    height: val(--heigth);
+    cursor: pointer;
+    border-radius: 5px;
+}
+
+input[type="range"]::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    width: 0px;
+    height: 0px;
+    background: black;
+    box-shadow: 1px 1px 7px #f3d559;
+    cursor: pointer;
+    box-shadow: -100vw 0 0 100vw #f3d559;
+}
+img{
+    object-fit: cover;
+    height: val(--heigth);
+    z-index: 1999;
+    position: absolute;
+    top: 0;
+}
+</style>
