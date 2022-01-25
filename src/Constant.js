57ed339f510b51e2587a305c1a714bd7e0704834
oajh04 <cloudtubekr@naver.com>
2022-01-26 00:23:55 +0900

    ⚡️ :: (#37) constant에 리뷰들들들 추가

--- src/Constant.js
+++ src/Constant.js
@@ -2,4 +2,6 @@ export default {
     PUSH_MENU_DATA: "pushMenuData",
     PUSH_MENU_ADD: "pushMenuAdd",
     SET_MENU_INFO: "setMenuInfo",
+    PUSH_REVIEW_DATA: "pushReviewData",
+    PUSH_REVIEW_ADD: "pushReviewAdd"
 }
