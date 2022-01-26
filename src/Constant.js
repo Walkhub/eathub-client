d16e1facfda6f75e1266cda2430f9596e9650d4f
oajh04 <cloudtubekr@naver.com>
2022-01-26 10:52:33 +0900

    ⚡️ :: (#40) 리뷰 스코어 소켓 연동

--- src/Constant.js
+++ src/Constant.js
@@ -3,5 +3,6 @@ export default {
     PUSH_MENU_ADD: "pushMenuAdd",
     SET_MENU_INFO: "setMenuInfo",
     PUSH_REVIEW_DATA: "pushReviewData",
-    PUSH_REVIEW_ADD: "pushReviewAdd"
+    PUSH_REVIEW_ADD: "pushReviewAdd",
+    SET_REVIEW_SCORE: "setReviewScore"
 }
