25a978d2d34212dc25fb87d18a959fd2c512ef4e
oajh04 <cloudtubekr@naver.com>
2022-01-27 09:33:23 +0900

    ⚡️ :: type 추가

--- src/Constant.js
+++ src/Constant.js
@@ -4,5 +4,8 @@ export default {
     SET_MENU_INFO: "setMenuInfo",
     PUSH_REVIEW_DATA: "pushReviewData",
     PUSH_REVIEW_ADD: "pushReviewAdd",
-    SET_REVIEW_SCORE: "setReviewScore"
+    SET_REVIEW_SCORE: "setReviewScore",
+    PUSH_MY_ORDER_FOOD: "pushMyOrderFood",
+    SET_ALL_ORDER_FOOD: "setAllOrderFood",
+    SET_MONEY: "setMoney"
 }
