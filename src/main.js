3888801cdd0444226a34b24db91bd9df44944b9b
oajh04 <cloudtubekr@naver.com>
2022-02-01 18:22:20 +0900

    ⚡️ :: axios use

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import './index.css'
 import router from './router'
 import store from './store'
+import VueAxios from 'vue-axios'
 import socketPlugin from './plugins/socketPlugin'
+import axios from 'axios'
 
 import App from './App.vue'
 const app = createApp(App);
@@ -10,5 +12,7 @@ const app = createApp(App);
 app.use(router)
 app.use(store)
 app.use(socketPlugin)
+app.use(VueAxios, axios)
+
 
 app.mount('#app');
